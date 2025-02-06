f0eb65d7c207d4289f88756c2c6fe204fd5800a6
Griffyn82 <gdavidson1982@gmail.com>
2025-02-06 03:24:14 +0000

    Create getlogs.js

---
+++ pages/api/getlogs.js
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;  // Use the environment variable
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+export default async function handler(req, res) {
+  try {
+    await client.connect();
+    const database = client.db('Whitman-Keypress');
+    const collection = database.collection('logs');
+    const logs = await collection.find({}).toArray();
+    res.status(200).json(logs);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch logs' });
+  } finally {
+    await client.close();
+  }
+}
